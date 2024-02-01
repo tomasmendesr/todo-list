ccfcbcecc14065cb3a2955849a42acebecbb421a
tomasmendesr <tomasmendesr@gmail.com>
2024-02-01 19:07:36 -0300

    create tasksummary component to show count of pending and complete tasks

---
+++ todo-list-ui/src/components/TaskSummary.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="summary">
+      <p class="summary-item">Completed tasks: {{ summary.completed }}</p>
+      <p class="summary-item">Pending tasks: {{ summary.notCompleted }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      summary: Object,
+    }
+}
+  </script>
+  
+  <style scoped>
+  .summary {
+    margin-top: 50px;
+  }
+  
+  .summary-item {
+    font-size: 1rem;
+  }
+  </style>
+  
