91a222e02b7babcbcc0f392c5f2ea9b54cf9ab11
tomasmendesr <tomasmendesr@gmail.com>
2024-02-01 19:08:15 -0300

    component to show list of tasks

---
+++ todo-list-ui/src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="task-list">
+      <TaskCard v-for="task in tasks" :key="task.id" :task="task" @delete-task="deleteTask" @mark-as-complete="markAsComplete"/>
+    </div>
+</template>
+  
+<script>
+  import TaskCard from './TaskCard.vue';
+  
+  export default {
+    props: {
+      tasks: Array,
+    },
+    components: {
+      TaskCard,
+    },
+    methods: {
+      deleteTask(id) {
+        this.$emit('delete-task', id);
+      },
+      markAsComplete(id) {
+        this.$emit('mark-as-complete', id)
+      }
+    }
+}
+</script>
+  
+  <style scoped>
+  .task-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+  </style>
+  
