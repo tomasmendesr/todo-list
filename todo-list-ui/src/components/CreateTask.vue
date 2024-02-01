5bc45ecf6fb8db15610269b0149fb0df132be979
tomasmendesr <tomasmendesr@gmail.com>
2024-02-01 10:21:59 -0300

    create frontend with vuejs

---
+++ todo-list-ui/src/components/CreateTask.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h2>Create new task</h2>
+      <form @submit.prevent="createTask">
+        <label for="description">Description:</label>
+        <input v-model="newTask.description" type="text" id="description" required>
+  
+        <label for="deadline">Deadline:</label>
+        <input v-model="newTask.deadline" type="datetime-local" id="deadline" required>
+  
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTask: {
+          description: '',
+          deadline: '',
+        },
+      };
+    },
+    methods: {
+      createTask() {
+        // api call
+      },
+    },
+  };
+  </script>
+  
---
+++ todo-list-ui/src/components/TaskList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Tasks</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.description }} - {{ task.deadline }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: Array,
+  },
+};
+</script>
